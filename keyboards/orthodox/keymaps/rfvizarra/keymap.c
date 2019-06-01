ff8060cfa5e46e8643fa2933b708cfbebd653f86
Rodrigo Fernandez-Vizarra <rfvizarra@gmail.com>
2019-06-01 21:27:28 +0200

    Tap dance to support Lower/LGUI in one key

--- keyboards/orthodox/keymaps/rfvizarra/keymap.c
+++ keyboards/orthodox/keymaps/rfvizarra/keymap.c
@@ -47,8 +47,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_QWERTY] = LAYOUT( \
     KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                                                                                KC_Y,   KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
-    KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                  KC_LGUI,      KC_LALT,            MO(_MEDIA)  , KC_DEL,               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
-    MO(_NAV),KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    MO(_LOWER),   KC_LSFT, CTL_T(KC_ENT),           KC_RALT,      KC_SPC, MO(_RAISE),   KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_LGUI \
+    KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                  KC_LGUI,      KC_LALT,            MO(_MEDIA)  , KC_RGUI,              KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
+    MO(_NAV),KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    MO(_LOWER),   KC_LSFT, CTL_T(KC_ENT),           KC_RALT,      KC_SPC, MO(_RAISE),   KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_DEL \
   ),
 
   [_COLEMAK] = LAYOUT(\
