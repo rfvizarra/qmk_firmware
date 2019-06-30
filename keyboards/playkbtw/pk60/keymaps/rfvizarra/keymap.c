62c696b44dc21e0738da1a9b90c044b04f5c7457
Rodrigo Fernandez-Vizarra <rfvizarra@gmail.com>
2019-06-30 15:08:58 +0200

    Added vi like layer

--- keyboards/playkbtw/pk60/keymaps/rfvizarra/keymap.c
+++ keyboards/playkbtw/pk60/keymaps/rfvizarra/keymap.c
@@ -1,29 +1,49 @@
 #include QMK_KEYBOARD_H
 
+/* Layers */
+#define _QUERTY 0
+#define _LAYER1 1
+#define _LAYER2 2
+#define _VILAYER 3
+
+/* Keycodes */
+#define MGKSPC LT(_VILAYER, KC_SPC)
+#define FORWRD LCTL(KC_RIGHT)
+#define BACKWRD LCTL(KC_LEFT)
+#define YANK LCTL(KC_INS)
+#define PASTE LSFT(KC_INS)
+
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-    [0] = LAYOUT_minila(
+    [_QUERTY] = LAYOUT_minila(
         KC_ESC,  KC_1,    KC_2,     KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_GRAVE, KC_BSPC,
         KC_TAB,  KC_Q,    KC_W,     KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
-        KC_CAPS, KC_A,    KC_S,     KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,
+        KC_LCTL, KC_A,    KC_S,     KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,
         KC_LSFT, KC_Z,    KC_X,     KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_LSFT, KC_UP,   KC_DEL,
-        KC_LCTL, KC_LGUI, KC_LALT,  MO(2),                              KC_SPC,  MO(1),   KC_RALT, KC_RCTL, KC_LEFT, KC_DOWN, KC_RIGHT
+        KC_LCTL, KC_LGUI, KC_LALT,  MO(2),                              MGKSPC,  MO(1),   KC_RALT, KC_RCTL, KC_LEFT, KC_DOWN, KC_RIGHT
     ),
 
-    [1] = LAYOUT_minila(
+    [_LAYER1] = LAYOUT_minila(
         RESET,   KC_F1,    KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______,
         _______, RGB_TOG,  RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, _______, _______, _______, _______, _______, _______,
         _______, RGB_MOD,  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
         _______, RGB_RMOD, _______, BL_DEC,  BL_TOGG, BL_INC,  _______, _______, _______, _______, _______, _______, KC_VOLU, _______,
-        _______, _______,  _______, _______,                            _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_MPLY 
+        _______, _______,  _______, _______,                            _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_MPLY
     ),
 
-    [2] = LAYOUT_minila(
+    [_LAYER2] = LAYOUT_minila(
         _______, KC_F1,    KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______,
         _______, RGB_TOG,  RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, _______, _______, _______, _______, _______, _______,
         _______, RGB_MOD,  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
         _______, RGB_RMOD, _______, BL_DEC,  BL_TOGG, BL_INC,  _______, _______, _______, _______, _______, _______, KC_PGUP, _______,
-        _______, _______,  _______, _______,                            _______, _______, _______, _______, KC_HOME, KC_PGDN, KC_END 
+        _______, _______,  _______, _______,                            _______, _______, _______, _______, KC_HOME, KC_PGDN, KC_END
+    ),
+    [_VILAYER] = LAYOUT_minila(
+        _______, _______,  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______,  _______,  FORWRD, _______, _______,    YANK, _______, _______, _______,   PASTE, _______, _______, _______,
+        _______, KC_HOME,  KC_PGDN, KC_PGUP,  KC_END, _______, KC_LEFT, KC_DOWN,   KC_UP,KC_RIGHT, _______, _______, _______,
+        _______, _______,   KC_DEL, _______, _______, BACKWRD, _______, _______, _______, _______, _______, _______, KC_PGUP, _______,
+        _______, _______,  _______, _______,                            _______, _______, _______, _______, KC_HOME, KC_PGDN, KC_END
     ),
 };
